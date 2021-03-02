efa449bf8ab10aaf54bffc5d67091a8a10bffeb8
laurenarthur <69876588+laurenarthur@users.noreply.github.com>
2021-03-01 20:41:06 -0500

    trying to fix discussion

--- src/components/Forum.jsx
+++ src/components/Forum.jsx
@@ -1,48 +1,15 @@
 import React from "react";
 
+
 function Forum() {
+
   return (
     <div className="forum"> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
+        <div class="container">
             <div class="main-body ">
               <div class="inner-wrapper">
-              <div class="inner-sidebar">
-                <div class="inner-sidebar-header justify-content-center"> 
-                  <button class="btn btn-primary has-icon btn-block" type="button" data-toggle="modal" data-target="#threadModal">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus mr-2">
-                          <line x1="12" y1="5" x2="12" y2="19"></line>
-                          <line x1="5" y1="12" x2="19" y2="12"></line>
-                      </svg>
-                    NEW DISCUSSION
-                  </button>
-                </div>
               
-                <div class="inner-sidebar-body p-0">
-                <div class="p-3 h-100" data-simplebar="init">
-                <div class="simplebar-wrapper" style={{margin: "-16px"}}>
-                <div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div>
-                        <div class="simplebar-mask">
-                            <div class="simplebar-offset" style={{right: "0px", bottom: "0px" }}>
-                                <div class="simplebar-content-wrapper" style={{height: "100%"}}>
-                                    <div class="simplebar-content" style={{padding: "16px"}}>
-                                        <nav class="nav nav-pills nav-gap-y-1 flex-column">
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon active">All Threads</a>
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon">Popular this week</a>
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon">Popular all time</a>
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon">Solved</a>
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon">Unsolved</a>
-                                            <a href="javascript:void(0)" class="nav-link nav-link-faded has-icon">No replies yet</a>
-                                        </nav>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="simplebar-placeholder" style={{width: "234px", height: "292px" }}></div>
-                    </div>
-                    <div class="simplebar-track simplebar-horizontal" style={{visibility: "hidden"}}><div class="simplebar-scrollbar" style={{width: "0px", display: "none"}}></div></div>
-                    <div class="simplebar-track simplebar-vertical" style={{visibility: "visible"}}><div class="simplebar-scrollbar" style={{height: "151px", display: "block"}}></div></div>
-                </div>
-                </div>    
-              </div>
 
               <div class="inner-main">
                 <div class="inner-main-header">
@@ -58,6 +25,19 @@ function Forum() {
                     <input type="text" class="form-control form-control-sm bg-gray-200 border-gray-200 shadow-none mb-4 mt-4" placeholder="Search forum" />
                 </span>
                 </div>
+              </div>
+              <div class="inner-sidebar">
+                <div class="inner-sidebar-header justify-content-center"> 
+                  <button class="btn btn-primary has-icon btn-block" type="button" data-toggle="modal" data-target="#threadModal"> 
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus mr-2">
+                          <line x1="12" y1="5" x2="12" y2="19"></line>
+                          <line x1="5" y1="12" x2="19" y2="12"></line>
+                      </svg>
+                    NEW DISCUSSION
+                  </button>
+                  
+                  
+              </div>
               </div>
 
               <div class="inner-main-body p-2 p-sm-3 collapse forum-content ">
@@ -80,9 +60,8 @@ function Forum() {
               </div>
             </div>
 
-
-            <div class="modal fade" id="threadModal" tabindex="-1" role="dialog" aria-labelledby="threadModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal" id="#threadModal" tabindex="-1" role="dialog" aria-labelledby="threadModalLabel" aria-hidden="true">
+             <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <form>
                     <div class="modal-header d-flex align-items-center bg-primary text-white">
@@ -99,7 +78,8 @@ function Forum() {
                         <textarea class="form-control summernote" style={{display: "none"}}></textarea>
 
                         <div class="custom-file form-control-sm mt-3" style={{width: "300px"}}>
-
+                            <input type="file" class="custom-file-input" id="customFile" multiple="" />
+                            <label class="custom-file-label" for="customFile">Attachment</label>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -109,6 +89,8 @@ function Forum() {
                 </form>
             </div>
         </div>
+    </div>
+
     </div>
     </div>
   );
