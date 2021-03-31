4df42c46a3eddc74ac04961da8c4f3359b6c6d08
Evan <ciriel@live.com>
2021-03-31 10:24:41 -0400

    Can pull questions from back end

--- src/components/Forum.jsx
+++ src/components/Forum.jsx
@@ -6,16 +6,21 @@ function Forum() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    
-    const [data, setData] = useState(null);
-    
-    const getData = () =>
-        fetch("https://crisis-octogon-3123.herokuapp.com/api/questions")
-        .then((res) => res.json())
-        .then(data => console.log(data));
+
+    const [data, setData] = useState([]);
+
+
+
+    //const getData = () =>
+      //  fetch("https://crisis-octogon-3123.herokuapp.com/api/questions")
+        //.then((res) => res.json())
+        //.then(data => console.log(data));
 
     useEffect(()=> {
-        getData().then((data) => setData(data))
+      fetch("https://crisis-octogon-3123.herokuapp.com/api/questions")
+      .then((response) => response.json())
+      .then(data => setData(data));
+      //getData().then((data) => setData(data))
     }, [])
 
 
@@ -24,7 +29,7 @@ function Forum() {
     <div class="container-fluid">
         <div class="inner-wrapper">
         <div class="inner-main">
-        
+
         <div class="align-items-center justify-content-between mb-4">
                 <Button variant="primary"  onClick={handleShow} class="btn btn-primary has-icon btn-block">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus mr-2">
@@ -34,7 +39,7 @@ function Forum() {
                     NEW DISCUSSION
                 </Button>
          </div>
-                
+
         <div class="inner-main-header">
         <div class="row">
             <div class="col-xl-8 col-lg-7 ">
@@ -55,33 +60,15 @@ function Forum() {
         <div class="inner-main-body">
         <div class="row">
             <div class="col-xl-6 md-4">
-                
-                    <div class="card mb-2">
-                    <div class="card-body p-2 p-sm-3">   
-                        <div class="media forum-item">
-                            <a href="#" data-target=".forum-content"></a>
-                            <div class="media-body">
-                                <h6><a href="#"data-target=".forum-content" class="text-body">Test Test</a></h6>
-                                <p class="text-body">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                </p>
-                                <p class="text-muted"><a class="text">drewdan</a> replied <span class="text-secondary font-weight-bold">13 minutes ago</span></p>
-                            </div>   
-                    </div>
-                    </div>
-                    </div>
+              <ul>
+              {data.map((item)=>{
+                  return <li key ={item.id}>{item.question}</li>
+              })}
+              </ul>
 
-                    {data?.map((item) =>
-                    <ul>
-                        <li>{item.questions}</li>
-                    </ul>
-                    
-                    )}
-
-                        </div>
-                    </div>  
-                </div>
+            </div>
+          </div>
+          </div>
 
         < div class="modal">
             <Modal size="lg" show={show} onHide={handleClose}>
@@ -99,7 +86,7 @@ function Forum() {
                                 <label for="threadTitle">Name</label>
                                 <input type="text" id="forum_post_name" required="required" class="form-control" placeholder="Enter name" autofocus="" />
                             </div>
-                        
+
                             <div class="form-group">
                                 <label for="threadTitle">Question</label>
                                 <input type="text" id="forum_post_body" required="required" class="form-control bg-light border-0 small" placeholder="Question?"></input>
